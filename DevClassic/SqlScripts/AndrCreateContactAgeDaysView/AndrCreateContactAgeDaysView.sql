fe8588c2427738795e2df71817b70c542a8bed32
Andre Belluci <andrebelluci@gmail.com>
2024-05-16 01:31:38 -0300

    Contact Age View Example Update

--- DevClassic/SqlScripts/AndrCreateContactAgeDaysView/AndrCreateContactAgeDaysView.sql
+++ DevClassic/SqlScripts/AndrCreateContactAgeDaysView/AndrCreateContactAgeDaysView.sql
@@ -1,4 +1,5 @@
 CREATE or ALTER view AndrVwContactAgeDays as
 select Id as AndrId, Name as AndrName, BirthDate as AndrBirthDate,
-datediff(day, BirthDate, getdate()) as AndrAgeDays
+datediff(day, BirthDate, getdate()) as AndrAgeDays,
+Id as AndrContactId
 from Contact
